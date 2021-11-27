c2399bdba588b12db4bce8c6add7e0b7ffa98769
Davi-Ga <davigaldinoky@gmail.com>
2021-11-26 22:20:38 -0300

    v0.1

--- Bloquim/src/App.java
+++ Bloquim/src/App.java
@@ -1,5 +1,26 @@
-public class App {
+
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application{
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("bloquin.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene tela = new Scene(root);
+
+        primaryStage.setTitle("Bloquim");
+        primaryStage.setScene(tela);
+        primaryStage.show();
     }
 }
