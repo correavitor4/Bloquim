2f0ba323220b72ee9f662a62be94eafc14368e19
Davi-Ga <davigaldinoky@gmail.com>
2021-11-26 22:20:55 -0300

    v0.1

---
+++ Bloquim/src/ControllerBloquim.java
@@ -0,0 +1,39 @@
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class ControllerBloquim {
+
+    @FXML
+    private Button Botaologin;
+
+    @FXML
+    private Button botaoCadastro;
+
+    @FXML
+    private TextField passLabel;
+
+    @FXML
+    private Button recuperarSenha;
+
+    @FXML
+    private TextField userLabel;
+
+    @FXML
+    void telaCadastro(ActionEvent event) {
+
+    }
+
+    @FXML
+    void telaInicial(ActionEvent event) {
+
+    }
+
+    @FXML
+    void telaRecuperaSenha(ActionEvent event) {
+
+    }
+
+}
