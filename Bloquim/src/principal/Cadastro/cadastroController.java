3b83c03186aabb6aaf9414f2ec523e15b4a711a9
Davi-Ga <davigaldinoky@gmail.com>
2021-12-02 20:22:18 -0300

    v0.1

---
+++ Bloquim/src/principal/Cadastro/cadastroController.java
@@ -0,0 +1,51 @@
+package principal.Cadastro;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class cadastroController {
+
+
+    @FXML
+    private PasswordField confirmaSenhaTextBox;
+
+    @FXML
+    private TextField emailTextBox;
+
+    @FXML
+    private Button feitoBtn;
+
+    @FXML
+    private TextField nomeTextBox;
+
+    @FXML
+    private PasswordField senhaTextBox;
+
+    @FXML
+    void confirmaSenhaUsuario(ActionEvent event) {
+
+    }
+
+    @FXML
+    void emailUsuario(ActionEvent event) {
+
+    }
+
+    @FXML
+    void finalizaCadastro(ActionEvent event) {
+
+    }
+
+    @FXML
+    void nomeUsuario(ActionEvent event) {
+
+    }
+
+    @FXML
+    void senhaUsuario(ActionEvent event) {
+
+    }
+
+}
