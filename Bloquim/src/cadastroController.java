ee252d5d00cecaf4161ab005489036924ba4f5ed
Davi-Ga <davigaldinoky@gmail.com>
2021-12-04 12:47:02 -0300

    v0.1

--- Bloquim/src/cadastroController.java
+++ Bloquim/src/cadastroController.java
@@ -3,6 +3,13 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import java.io.IOException;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 
 public class cadastroController {
 
@@ -22,6 +29,9 @@ public class cadastroController {
     private PasswordField senhaTextBox;
 
     @FXML
+    private Button voltarBtn;
+
+    @FXML
     void confirmaSenhaUsuario(ActionEvent event) {
 
     }
@@ -33,7 +43,7 @@ public class cadastroController {
 
     @FXML
     void finalizaCadastro(ActionEvent event) {
-        
+
     }
 
     @FXML
@@ -44,6 +54,15 @@ public class cadastroController {
     @FXML
     void senhaUsuario(ActionEvent event) {
 
+    }
+
+    @FXML
+    void voltaTela(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        Stage loginStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene loginScene = new Scene(root);
+        loginStage.setScene(loginScene);
+        loginStage.show();
     }
 
 }
--- Bloquim/src/loginController.java
+++ Bloquim/src/loginController.java
@@ -8,11 +8,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
 
 public class loginController {
 
