bd89c91c0579c420c61cb6a20f14133c4642a4ad
Davi-Ga <davigaldinoky@gmail.com>
2021-12-08 21:21:13 -0300

    v0.1

--- Bloquim/src/loginController.java
+++ Bloquim/src/loginController.java
@@ -2,58 +2,67 @@ import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import javafx.scene.control.PasswordField;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+
 
 public class loginController {
 
     @FXML
-    private Button cadastroBtn;
+    private Button cadastreSeBtn;
+
+    @FXML
+    private Button esqueciSenhaBtn;
 
     @FXML
     private Button feitoBtn;
 
     @FXML
-    private TextField nomeTextField;
-
-    @FXML
-    private Button senhaPerdida;
-
-    @FXML
     private PasswordField senhaTextBox;
 
     @FXML
-    void irCadastro(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("telacadastro.fxml"));
-        Stage cadastroStage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        Scene cadastroscene = new Scene(root);
-        cadastroStage.setScene(cadastroscene);
-        cadastroStage.show();
-    }
+    private TextField usuarioTextBox;
 
     @FXML
-    void loginUsuario(ActionEvent event) {
-
-    }
+    private Button xBtn;
 
     @FXML
-    void realizaLogin(ActionEvent event) {
+    void entrarApp(ActionEvent event) {
 
     }
 
     @FXML
-    void recuperaSenha(ActionEvent event) {
+    void fechaAba(ActionEvent event) {
+
+    }
+
+    @FXML
+    void nomeUsuario(ActionEvent event) {
+
+    }
+
+    @FXML
+    void redefineSenha(ActionEvent event) {
 
     }
 
     @FXML
     void senhaUsuario(ActionEvent event) {
 
+    }
+
+    @FXML
+    void vaiTelaCadastro(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("telacadastro.fxml"));
+        Stage cadastroStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene cadastroscene = new Scene(root);
+        cadastroStage.setScene(cadastroscene);
+        cadastroStage.show();
     }
 
 }
