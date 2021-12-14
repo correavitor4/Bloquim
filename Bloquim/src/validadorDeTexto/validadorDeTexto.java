ecf8cc1432cab776d0d5e4722cda69647e769822
João Vitor Oliveira Corrêa <correavitor4@hotmail.com>
2021-12-14 11:52:59 -0300

    v0.1

---
+++ Bloquim/src/validadorDeTexto/validadorDeTexto.java
@@ -0,0 +1,34 @@
+package validadorDeTexto;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class validadorDeTexto {
+
+    public static boolean validarEmail(String emailText) {
+        Boolean resposta = false;
+        if(emailText.length()>0 && emailText!=null){
+            String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+            Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+            Matcher matcher = pattern.matcher(emailText);
+            if (matcher.matches()) {
+                resposta = true;
+            }
+        }
+        return resposta;
+    }
+
+    public static boolean validarSenha(String senhaText) {
+        boolean resposta = false;
+        if(senhaText.length()>8 && senhaText!=null){
+            resposta=true;
+        }
+        return resposta;
+    }
+    /*
+    public static boolean validarUsuario() {
+        boolean resposta = false;
+        String expressao= "[\w]";
+    }
+    */
+}
+
