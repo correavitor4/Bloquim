feedf5a142e86eafbd2737e66952d11650fa1fdd
João Vitor Oliveira Corrêa <correavitor4@hotmail.com>
2021-12-14 11:53:14 -0300

    v0.1

--- Bloquim/src/App.java
+++ Bloquim/src/App.java
@@ -13,11 +13,7 @@ public class App extends Application{
     @Override
     public void start(Stage primaryStage) throws Exception {
         
-<<<<<<< HEAD
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("telalogin.fxml"));
-=======
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/FXML/Login.fxml"));
->>>>>>> 664ee64742e47ebe5dce6a2277022a4e5504b52a
         Parent root = fxmlLoader.load();
         Scene tela = new Scene(root);
 
